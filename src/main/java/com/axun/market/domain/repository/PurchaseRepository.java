046ea346a30eb61858169d1a80dbd435abe0707d
romudev <dev.2013.03@gmail.com>
2022-04-28 23:50:49 -0500

    first commit

---
+++ src/main/java/com/axun/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.axun.market.domain.repository;
+
+import com.axun.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
